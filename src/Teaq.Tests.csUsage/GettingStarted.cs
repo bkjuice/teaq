25f962ddc829c874096a7c2018b202c8c4573fff
bruce krakower <bruce.krakower@gmail.com>
2016-08-04 06:59:35 -0400

    Doc upload.

---
+++ src/Teaq.Tests.csUsage/GettingStarted.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Teaq.Tests.csUsage
+{
+    [TestClass]
+    public class GettingStarted
+    {
+        [TestMethod]
+        public void UseRepository()
+        {
+            ////Repository.Default;
+                
+                
+                
+                
+        
+                
+                
+                
+                
+                
+                
+                
+                
+        }
+    }
+}
