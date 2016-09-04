f5ca5f62ed2dbb53e7d6035df8ded610e9e2c683
bruce krakower <bruce.krakower@gmail.com>
2016-09-04 13:39:16 -0400

    Consolidation of command extension methods.

--- src/Teaq/DbCommandExtensions.cs
+++ src/Teaq/DbCommandExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 using Teaq.Configuration;
 using Teaq.QueryGeneration;
 
@@ -30,16 +32,16 @@ namespace Teaq
         /// <returns>The current command instance.</returns>
         public static IDbCommand Open(this IDbCommand command)
         {
-            if (command.Connection.State != ConnectionState.Open)
+            if (command.Connection?.State != ConnectionState.Open)
             {
-                command.Connection.Open();
+                command.Connection?.Open();
             }
 
             return command;
         }
 
         /// <summary>
-        /// Creates a command clean-up callback.
+        /// Creates a command clean-up callback closure.
         /// </summary>
         /// <param name="command">The command.</param>
         /// <returns>The cleanup action to use when enumerating results.</returns>
@@ -50,6 +52,64 @@ namespace Teaq
                 command?.Connection?.Close();
                 command?.Dispose();
             };
+        }
+
+        /// <summary>
+        /// Executes the provide command asynchronously and returns an awaitable data reader instance.
+        /// </summary>
+        /// <param name="command">The command to execute.</param>
+        /// <param name="commandBehavior">The command behavior to use.</param>
+        /// <returns>
+        /// An awaitable data reader to read.
+        /// </returns>
+        public static async Task<IDataReader> ExecuteReaderAsync(this IDbCommand command, CommandBehavior commandBehavior = CommandBehavior.Default)
+        {
+            var sqlCommand = command as SqlCommand;
+            if (sqlCommand != null)
+            {
+                return await sqlCommand.ExecuteReaderAsync(commandBehavior);
+            }
+
+            Func<IDataReader> awaitable = () => command.ExecuteReader(commandBehavior);
+            return await awaitable.InvokeAsAwaitable();
+        }
+
+        /// <summary>
+        /// Executes the command against the backing store asynchronously.
+        /// </summary>
+        /// <param name="command">The command to execute.</param>
+        /// <returns>
+        /// The awaitable number of rows affected.
+        /// </returns>
+        public static async Task<int> ExecuteNonQueryAsync(this IDbCommand command)
+        {
+            var sqlCommand = command as SqlCommand;
+            if (sqlCommand != null)
+            {
+                return await sqlCommand.ExecuteNonQueryAsync();
+            }
+
+            Func<int> awaitable = () => command.ExecuteNonQuery();
+            return await awaitable.InvokeAsAwaitable();
+        }
+
+        /// <summary>
+        /// Executes the command against the backing store asynchronously.
+        /// </summary>
+        /// <param name="command">The command to execute.</param>
+        /// <returns>
+        /// The awaitable first value returned in the first row.
+        /// </returns>
+        public static async Task<object> ExecuteScalarAsync(this IDbCommand command)
+        {
+            var sqlCommand = command as SqlCommand;
+            if (sqlCommand != null)
+            {
+                return await sqlCommand.ExecuteScalarAsync();
+            }
+
+            Func<object> awaitable = () => command.ExecuteScalar();
+            return await awaitable.InvokeAsAwaitable();
         }
     }
 }
